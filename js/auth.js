f96381ee92f6b52a0064a1d2cb7a60275accc6e8
PopularAtacarejo <popularatacarejomkt@gmail.com>
2025-11-10 09:25:28 -0300

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,5 @@
+// Gerenciamento de Autenticação (se necessário no futuro)
+const Auth = {
+    // Funções de autenticação podem ser adicionadas aqui
+    // Por enquanto, usamos token fixo no utils.js
+};
